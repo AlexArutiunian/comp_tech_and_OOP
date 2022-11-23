854a5010f7acce8f020ce04056b32d19947df0ac
AlexArutiunian <arutunanalexandr17@gmail.com>
2022-11-23 15:27:47 +0300

    two process with pipe

---
+++ 5sem/answers/05-3/1st.c
@@ -0,0 +1,55 @@
+// Первая программа. Её необходимо запускать.
+// Данная программа выводит в консоль информацию, которая была занесена в pipe() второй программой.
+
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <stdio.h>
+#include <string.h>
+ 
+int main( int argc, char *argv[]) {
+    char *locale = setlocale(LC_ALL, "");
+
+    int fd[2], newProc;
+    size_t size;
+    char str[14];
+
+    // для передачи значения дескриптора в другую программу необходимо int преобразовывать в char
+    char fd0[11], fd1[11];
+
+    if(pipe(fd) < 0) {
+        printf("Не получилось создать pipe\n");
+        exit(-1);
+    }
+
+    newProc = fork();
+
+    if (newProc < 0) {
+        printf("Не получилось создать процесс-ребёнка\n");
+        exit(-1);
+
+    } else if (newProc > 0) {
+        // Внутри родительского процесса
+        // Закрытие выходного потока данных
+        close(fd[1]);
+
+        read(fd[0], str, 14);
+        
+        // Закрытие входного потока данных
+        close(fd[0]);
+        printf ("Результат чтения: %s\n", str);
+
+    } else {
+        // Внутри порождённого процесса
+        // Запись первого аргумента в массив символов
+        sprintf(fd0, "%d", fd[0]);
+        
+        //Запись второго аргумента в массив символов
+        sprintf(fd1, "%d", fd[1]);
+        
+        execl("./2nd", fd0, fd1, NULL);
+    }
+
+    return 0;
+}
---
+++ 5sem/answers/05-3/2nd.c
@@ -0,0 +1,35 @@
+// Вторая программа. Данная программа ответственна за занесение данных в pipe()
+
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <stdio.h>
+#include <string.h>
+ 
+int main(int argc, char* argv[])
+{
+    char *locale = setlocale(LC_ALL, "");
+
+    int fd[2];
+    size_t size;
+    char str[14]={"Program test!"};
+ 
+    // Аргументы приходят в виде const char*. Необходимо преобразовать в число. Заведомо известно, что там кроме чисел ничего быть не может
+    fd[0] = atoi(argv[0]);
+    fd[1] = atoi(argv[1]);
+ 
+    // Закрытие входного потока данных
+    close(fd[0]);
+    size=write(fd[1], str, 14);
+    if (size!=14)
+    {
+        printf("%zu", size);
+        printf("Строка не может быть прочитана полностью\n");
+        exit(-1);
+    }
+    
+    // Закрытие выходного потока данных
+    close(fd[1]);
+    return 0;
+}
